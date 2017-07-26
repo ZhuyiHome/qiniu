d738b79480b5d3d4d1b9abd45d37c52ee7b86835
Wade Xing <xing.xing@condenast.com.cn>
2017-07-26 19:32:50 +0800

    fix: avthumb must be the 1st arg in trans_fops fun

--- lib/qiniu/fop/av.ex
+++ lib/qiniu/fop/av.ex
@@ -23,19 +23,17 @@ defmodule Qiniu.Fop.AV do
 
 
   @doc """
-   iex> Qiniu.Fop.AV.trans_fops([])
-   ""
    iex> Qiniu.Fop.AV.trans_fops([avthumb: "mp4", s: "640x360", saveas: "bucket1:test.mp4"])
-   "/avthumb/mp4/s/640x360|saveas/YnVja2V0MTp0ZXN0Lm1wNA=="
+   "avthumb/mp4/s/640x360|saveas/YnVja2V0MTp0ZXN0Lm1wNA=="
   """
-  def trans_fops(opts \\ []) do
+  def trans_fops([{:avthumb, avthumb} | opts]) do
     opts
-    |> Enum.reduce("", fn({arg, value}, acc) ->
+    |> Enum.reduce("avthumb/#{avthumb}", fn({arg, value}, acc) ->
       case trans_fop(arg, value) do
         "|" <> _ = next_step -> "#{acc}#{next_step}"
         regular_fop  -> "#{acc}/#{regular_fop}"
       end
-    end)
+    end) |> IO.inspect
   end
   defp trans_fop(:saveas, bucket_name_key) do
     "|saveas/" <> Base.url_encode64(bucket_name_key)
